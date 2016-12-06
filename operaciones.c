4d8b2d1c961340741881130195106b4f550c47f8
Paco <ande.esta.paco@protonmail.com>
2016-12-06 21:09:55 +0100

    Funcion nueva: resta. Llamada en main.

--- operaciones.c
+++ operaciones.c
@@ -5,14 +5,20 @@ int sumar(int a, int b){
   return a + b;
 }
 
+int restar(int a, int b){
+  return a - b;
+}
+
 int main(int argc, char const *argv[]){
 
   // OPERACIONES
-  int suma;
+  int suma, resta;
 
   suma = sumar(10, 5);
+  resta = restar(10, 3);
 
   printf("Suma: %d\n", suma);
+  printf("Resta: %d\n", resta);
 
   return 0;
 
