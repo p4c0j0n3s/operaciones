4d7cd8d08befcf17ff2a85c9462bfc2f171f1f86
Paco <ande.esta.paco@protonmail.com>
2016-12-12 01:58:06 +0100

    Nueva funcion: dividir. Llamada en main.

--- operaciones.c
+++ operaciones.c
@@ -2,19 +2,22 @@
 #include "multiplicar.h"
 #include "restar.h"
 #include "sumar.h"
+#include "dividir.h"
 
 int main(int argc, char const *argv[]){
 
   // OPERACIONES
-  int suma, resta, multiplicacion;
+  int suma, resta, multiplicacion, division;
 
   suma = sumar(10, 5);
   resta = restar(10, 3);
   multiplicacion = multiplicar(5, 7);
+  division = dividir(10,2);
 
   printf("Suma: %d\n", suma);
   printf("Resta: %d\n", resta);
   printf("Multiplicacion: %d\n", multiplicacion);
+  printf("Division: %d\n", division);
 
   return 0;
 
